9e7b0640d3d25c80b1dd0de7fa85bc8be5eac8cc
Nala Mahen <mail2mahen@yahoo.co.uk>
2020-12-10 10:51:03 +0000

    initial commit

---
+++ client/src/Fib.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Fib = () => {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState('');
+
+  const fetchValues = async () => {
+    const response = await axios.get('/api/values/current');
+    setValues(response.data);
+  };
+
+  const fetchIndexes = async () => {
+    const response = await axios.get('/api/values/all');
+    setSeenIndexes(response.data);
+  };
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index,
+    });
+
+    setIndex('');
+  };
+
+  const renderSeenIndexes = () => {
+    return seenIndexes.map(({ number }) => number);
+  };
+
+  const renderValues = () => {
+    const entries = [];
+
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index</label>
+        <input
+          value={index}
+          onChange={(event) => setIndex(event.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen</h3>
+      {renderSeenIndexes()}
+
+      <h3>Calculated Values:</h3>
+      {renderValues()}
+    </div>
+  );
+};
+
+export default Fib;
